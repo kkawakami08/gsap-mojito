444ec0fb7d1edaddf2e5f12967c43db1e1fc0c99
Kaori Kawakami <k.kawakami08@gmail.com>
2025-07-04 17:59:17 -0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-screen">
-      <h1 className="text-7xl">Hi</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
